423117f341d5d46cf54db05d1e9f6305581bd222
Mahesh <abbanaboinamahesyadav@gmail.com>
2026-09-05 14:53:05 +0530

    Passbyvalue

---
+++ Day-18/passbyvalue.py
@@ -0,0 +1,32 @@
+'''l = [1,2,3,4,5]
+sum=15
+max=20
+print(max)'''
+
+#int float str list tuple set dict bool 
+#int float str tuple bool -immutable values-pass by value
+#list set dict-mutable values -pass by refernce
+
+#int
+'''def display(n):
+    n= n+10
+    print("Inside:",n)
+n=10
+display(n)
+print("Outside:",n)'''
+
+#float
+'''def display(n):
+    n += 10.5
+    print("Inside:",n)
+n=11.5
+display(n)
+print("Outside:",n)'''
+
+#str
+'''def display(n):
+    n += "lang"
+    print("Inside:",n)
+n="python"
+display(n)
+print("Outside:",n)'''
