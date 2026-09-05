614f460029086c2772b1258f89481c9cecaec37c
Mahesh <abbanaboinamahesyadav@gmail.com>
2026-09-05 14:45:06 +0530

    Assert_pass And Patterns

---
+++ Day-15/patterns.py
@@ -0,0 +1,42 @@
+'''for i in range(20):
+    for j in range(10):
+        print('*', end = " ")
+    print()'''
+
+'''for row in range(5):
+    for column in range(5):
+        print(row,end=' ')
+    print()'''
+
+'''for row in range(5):
+    for col in range(5):
+        print(col,end = ' ')
+    print()'''
+
+'''for i in range(5):
+    for j in range(5):
+        print(i+j,end= ' ')
+    print()'''
+
+'''for i in range(5):
+    for j in range(5):
+        print(j%2,end = ' ')
+    print()'''
+
+'''for i in range(5):
+    for j in range(5):
+        print((i+j)%2,end = ' ')
+    print()'''
+
+'''for i in range(5):
+    for j in range(i+1):
+        print('*',end = ' ')
+    print()'''
+
+'''for i in range(5):
+    for j in range(5-i):
+        print('*',end = ' ')
+    print()'''
+
+
+
