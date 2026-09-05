e3ae295252565946804a4f364f9556224dd0ea52
Mahesh <abbanaboinamahesyadav@gmail.com>
2026-09-05 14:35:12 +0530

    Forloop and nestedif

---
+++ Day-12/forloop.py
@@ -0,0 +1,66 @@
+#str list tuple set dict range()
+'''
+for var in seq:
+print(var)
+'''
+'''s= 'codegnan'
+for ch in s:
+    print("s")'''
+
+'''s= 'Mahesh'
+for ch in s:
+    if ch in 'aeiouAEIOU':
+        print(ch)'''
+
+
+#even or odd
+'''l =[12,34,56,7,823,53,454,234,]
+for i in l:
+ifi%2==0:
+ print(i,"even")
+else:
+ print(i,"Odd")'''
+
+#marks tuple
+'''marks=(90,45,23,67,22,45,12)
+for mark in marks:
+    if mark>35:
+        print(mark,"pass")
+    else:
+        print(mark,"fail")'''
+
+#list of follwers
+'''followers = {'mahesh','venu','nandu','vivek'}
+for i in followers:
+    print(i)'''
+
+#dict
+'''bus = {'s1':'booked','s2':'available','s3':'available','s4':'available'}
+for seat in bus:
+    if bus.get(seat)=='available':
+      print(seat,bus.get(seat))'''
+#range(start,end+1,step)=>(0,nodef,1)
+'''for i in range(1,11):
+ print(i)'''
+#even
+'''for i in range(2,51,2):
+    print(i)'''
+
+'''for i in range(1,100,2):
+    print(i,end=' ')'''
+
+'''for i in range(5,51,5):
+    print(i,end= ' ')'''
+
+'''n = int(input("enter the table no:"))
+for i in range(1,11):
+    print(f'{n} * {i} = {n*i}')'''
+
+
+
+    
+
+
+ 
+
+    
---
+++ Day-12/nestedif.py
@@ -0,0 +1,28 @@
+'''fa = eval(input("Follow Account"))
+cf = eval(input("Close friend"))
+if fa:
+    if cf:
+        print("story visible")
+    else:
+     print("not in close friend list")
+else:
+     print("follow the account first")'''
+'''reg = eval(input())
+fee = eval(input())
+if reg:
+    if fee:
+        print("tournament Entry Confirmed")
+    else:
+        print("Entry Fee Pending")
+else:
+    print("Registration required")'''
+
+'''link = eval(input())
+per = eval(input())
+if link:
+    if per:
+        print("File Opened Successfully")
+    else:
+        print("Access Denied")
+else:
+    print("Invalid File Link")'''
