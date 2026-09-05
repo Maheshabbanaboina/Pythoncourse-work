a237f7121e4488262f502ae5025aee1a8bd26875
Mahesh <abbanaboinamahesyadav@gmail.com>
2026-09-05 14:47:38 +0530

    Functions

---
+++ Day-17/functions.py
@@ -0,0 +1,49 @@
+#functions
+'''def function_name()#function defination
+
+function body:
+
+
+function_name()#function call'''
+
+
+
+#imagine an e-commerce
+'''def calculate_gst(amount):
+    gst = amount * 0.18
+    print("GST:",gst)
+calculate_gst(1000)
+calculate_gst(5000)'''
+
+'''def func():#function defination
+    print("this is function")#function body
+func()#function call'''
+
+#types of functions
+#bulit-in functions
+'''print("hello")
+print(10)
+print("My age is",21)'''
+
+'''name = "python"
+print(len(name))'''
+
+'''numbers = [10,20,30,45]
+print(len(numbers))'''
+
+'''numbers = [1,2,3,4,56]
+print(max(numbers))'''
+
+'''print(round(4.66))'''
+
+'''print(abs(-15))'''
+
+'''numbers = [.55,68,32,10,30,40,50]
+print(sorted(numbers))'''
+
+
+
+
+
+
+
